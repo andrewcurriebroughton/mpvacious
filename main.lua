0754173f4edb57bc51dbb0e8c9dedaf9ce5d092b
Andrew Broughton <andrew.currie.broughton@gmail.com>
2024-02-02 14:37:31 -0500

    testing out some interop

--- main.lua
+++ main.lua
@@ -3,9 +3,12 @@ require('subs2srs')
 local mp = require('mp')
 local utils = require('mp.utils')
 
-local function post_subtitle(name, value)
-    print('NAME:', name, type(name))
-    local subtitle_start = value
+local function construct_curl_command(data, route)
+    local url = string.format('http://127.0.0.1:5000%s', route)
+    return string.format("curl -X POST -H 'Content-Type: application/json' -d '%s' %s", utils.format_json(data), url)
+end
+
+local function post_subtitle(_, subtitle_start)
     if subtitle_start == nil then
         return
     end
@@ -15,10 +18,21 @@ local function post_subtitle(name, value)
     data['subtitleStart'] = subtitle_start
     data['subtitleEnd'] = subtitle_end
     data['subtitleText'] = subtitle_text
-    local curl_command = string.format("curl -X POST -H 'Content-Type: application/json' -d '%s' %s", utils.format_json(data), 'http://127.0.0.1:5000/subtitle')
+    local curl_command = construct_curl_command(data, '/subtitle')
     os.execute(curl_command)
 end
 mp.observe_property('sub-start', 'string', post_subtitle)
+
+local function post_primary_subtitles_path(_, primary_subtitles_path)
+    if primary_subtitles_path == nil then
+        return
+    end
+    local data = {}
+    data['primarySubtitlesPath'] = primary_subtitles_path
+    local curl_command = construct_curl_command(data, '/subtitles/load/primary')
+    os.execute(curl_command)
+end
+mp.observe_property('current-tracks/sub/external-filename', 'string', post_primary_subtitles_path)
 
 -- confirm `sub-start` is `string` type (to pass to `mp.observe_property`):
 -- mp.add_forced_key_binding('z', display_subtitle_start_property)
